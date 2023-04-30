1c95f929991551ebb1fb7778d38feec080163a4e
«Pavel <kskles@yandex.ru>
2023-04-30 13:50:31 +0700

    Задача19.02 корр

--- Tesk19/Program.cs
+++ Tesk19/Program.cs
@@ -6,15 +6,7 @@
 // 
  Console.WriteLine("Введите пятизначное число");
  string? str = Console.ReadLine();
- Console.ReadLine();
-
-   //string str = "Простая строка";
-
-        // Получить длину строки и 6й символ в строке используя индексатор
-        //Console.WriteLine("Длина строки - {0}, 6й символ - '{1}'",
-          //  str.Length, str[5]);
-   // }
-//}     
+ Console.ReadLine();   
         if ((str.Length) <= 5)
         {
          int num1 = Convert.ToInt32(Convert.ToString(str[0]));
@@ -22,9 +14,9 @@
          int num5 = Convert.ToInt32(Convert.ToString(str[4]));
             Console.WriteLine(num5);
           int num2 = Convert.ToInt32(Convert.ToString(str[1]));
-         Console.WriteLine(num2);
+             Console.WriteLine(num2);
          int num4 = Convert.ToInt32(Convert.ToString(str[3]));
-         Console.WriteLine(num4);
+              Console.WriteLine(num4);
 
          if(num1==num5 || num2==num4)
          {
@@ -41,8 +33,3 @@
          }
 
 
-//}
- //else
-//{
-//Console.WriteLine("Число не сответствует заданным параметрам");
-//}
